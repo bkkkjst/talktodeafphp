3f536204b6bdac364cdcaa48140d55924f5abfab
bkkkjst <bkk_kjst02279@hotmail.com>
2015-02-02 01:44:25 +0700

    php
    test php to server red hat

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+@mysql_connect("localhost","root","");
+mysql_select_db("t2d_db");
+
+//ดึงข้อมูลออกมาในรูปแบบ UTF 8
+mysql_query("SET NAMES UTF8");
+
+$q=mysql_query("SELECT vocabulary.id, "
+			."vocabulary.voc_name,"
+			."vocabulary.voc_des,"
+			."video.vid_name,"
+			."type.type_name,"
+			."image.img_name,"
+			."example.exam,"
+			."category.name_cat,"
+			."image.img_path,"
+			."video.vid_path "
+			."FROM "
+			."vocabulary ,video ,type ,image ,example ,category WHERE vocabulary.video_id = video.id AND vocabulary.category_id = category.id AND vocabulary.type_id = type.id AND vocabulary.example_id = example.id AND vocabulary.image_id = image.id");
+
+while($e=mysql_fetch_assoc($q))
+       $output[]=$e;
+
+function jsonRemoveUnicodeSequences($struct) {
+   return @preg_replace("/\\\\u([a-f0-9]{4})/e", "iconv('UCS-4LE','UTF-8',pack('V', hexdec('U$1')))", json_encode($struct));
+}   
+   
+//print(json_encode($output));
+print jsonRemoveUnicodeSequences($output);
+mysql_close();
+?>
+

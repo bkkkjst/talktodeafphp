257d396f58d7365a1aa770f61af4752efe9ff5ec
bkkkjst <bkk_kjst02279@hotmail.com>
2015-02-02 23:29:13 +0700

    add information_all.php

---
+++ information_all.php
@@ -0,0 +1,28 @@
+<?php
+mysql_connect("127.10.75.2:3306","adminqALkDCf","reUky8W-Z5DB");
+mysql_select_db("t2d_db");
+
+//ดึงข้อมูลออกมาในรูปแบบ UTF 8
+mysql_query("SET NAMES UTF8");
+
+$q=mysql_query("SELECT\n".
+"information.id,\n".
+"information.place_name,\n".
+"information.address,\n".
+"information.lat,\n".
+"information.phone,\n".
+"information.`long`\n".
+"FROM\n".
+"information");
+			
+while($e=mysql_fetch_assoc($q))
+       $output[]=$e;
+
+function jsonRemoveUnicodeSequences($struct) {
+   return @preg_replace("/\\\\u([a-f0-9]{4})/e", "iconv('UCS-4LE','UTF-8',pack('V', hexdec('U$1')))", json_encode($struct));
+}   
+   
+//print(json_encode($output));
+print jsonRemoveUnicodeSequences($output);
+mysql_close();
+?>

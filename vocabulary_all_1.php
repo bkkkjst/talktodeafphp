b0c99ca2535537995550d2f5345923eb1a72afe1
bkkkjst <bkk_kjst02279@hotmail.com>
2015-02-02 22:39:47 +0700

    edit voc_all_1

--- vocabulary_all_1.php
+++ vocabulary_all_1.php
@@ -5,31 +5,15 @@ mysql_select_db("t2d_db");
 //ดึงข้อมูลออกมาในรูปแบบ UTF 8
 mysql_query("SET NAMES UTF8");
 
-$q=mysql_query("SELECT "
-			."vocabulary.id, "
-			."vocabulary.voc_name, "
-			."vocabulary.voc_des, "
-			."video.vid_name, "
-			."video.vid_path, "
-			."type.type_name, "
-			."image.img_name, "
-			."image.img_path, "
-			."example.exam, "
-			."category.name_cat "
-			."FROM "
-			."vocabulary , "
-			."video , "
-			."type , "
-			."image , "
-			."example , "
-			."category "
-			."WHERE "
+$q=mysql_query("SELECT vocabulary.id, vocabulary.voc_name, vocabulary.voc_des, video.vid_name, video.vid_path, type.type_name, image.img_name, "
+			."image.img_path, example.exam, category.name_cat "
+			." FROM "
+			."vocabulary , video , type , image , example , category "
+			." WHERE "
 			."t2d_db.vocabulary.video_id = t2d_db.video.id AND "
-			."vocabulary.type_id = type.id AND "
-			."vocabulary.image_id = image.id AND "
-			."vocabulary.example_id = example.id AND "
+			."vocabulary.type_id = type.id AND vocabulary.image_id = image.id AND vocabulary.example_id = example.id AND "
 			."vocabulary.category_id = category.id AND "
-			."vocabulary.id = 1 ");
+			."vocabulary.id = 1");
 			
 while($e=mysql_fetch_assoc($q))
        $output[]=$e;

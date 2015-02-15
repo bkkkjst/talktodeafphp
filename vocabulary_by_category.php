acabcb4dbaf806f27f16514ec748e4b9f76c5775
bkkkjst <bkk_kjst02279@hotmail.com>
2015-02-15 16:45:11 +0700

    edit vocabulary_by_category.php

--- vocabulary_by_category.php
+++ vocabulary_by_category.php
@@ -7,13 +7,14 @@ mysql_query("SET NAMES UTF8");
 $cat_name =$_GET['cat_name'];
 
 $q=mysql_query("SELECT\n".
-"vocabulary.voc_name\n".
+"vocabulary.id,\n".
+"vocabulary.voc_name,\n".
+"video.vid_name\n".
 "FROM\n".
 "vocabulary ,\n".
-"category\n".
+"video\n".
 "WHERE\n".
-"category.cat_name = '".$cat_name."' AND\n".
-"vocabulary.category_id = category.id");
+"vocabulary.video_id = video.id");
 while($e=mysql_fetch_assoc($q))
        $output[]=$e;
 

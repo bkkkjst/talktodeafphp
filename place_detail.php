a885c3e9d284c7cd4cd661c83b0a44a21fcf4d45
bkkkjst <bkk_kjst02279@hotmail.com>
2015-02-15 20:38:24 +0700

    add place_detail.php & place_by_name.php

---
+++ place_detail.php
@@ -0,0 +1,31 @@
+<?php
+include("connection.php");
+?>
+
+<?php
+mysql_connect($hostname,$username,$password);
+mysql_select_db($db_name);
+
+//ดึงข้อมูลออกมาในรูปแบบ UTF 8
+mysql_query("SET NAMES UTF8");
+
+$q=mysql_query("SELECT\n".
+"place.id,\n".
+"place.place_name,\n".
+"place.address,\n".
+"place.phone,\n".
+"place.`long`,\n".
+"place.lat\n".
+"FROM\n".
+"place");
+while($e=mysql_fetch_assoc($q))
+       $output[]=$e;
+
+function jsonRemoveUnicodeSequences($struct) {
+   return @preg_replace("/\\\\u([a-f0-9]{4})/e", "iconv('UCS-4LE','UTF-8',pack('V', hexdec('U$1')))", json_encode($struct));
+}   
+   
+//print(json_encode($output));
+print jsonRemoveUnicodeSequences($output);
+mysql_close();
+?>

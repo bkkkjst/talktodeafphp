adf5cf7cd7f9f91cd472a1f934d5fb9a84338705
bkkkjst <bkk_kjst02279@hotmail.com>
2015-02-17 18:36:01 +0700

    create file search_category.php

---
+++ search_category.php
@@ -0,0 +1,32 @@
+<?php
+include("connection.php");
+?>
+
+<?php
+mysql_connect($hostname,$username,$password);
+mysql_select_db($db_name);
+
+//ดึงข้อมูลออกมาในรูปแบบ UTF 8
+mysql_query("SET NAMES UTF8");
+$cate_name = $_GET['cate_name'];
+
+$q=mysql_query("SELECT\n".
+"category.cat_name\n".
+"FROM\n".
+"category\n".
+"WHERE\n".
+"category.cat_name LIKE '%{$cate_name}' OR\n".
+"category.cat_name = '{$cate_name}%' OR\n".
+"category.cat_name = '%{$cate_name}%'");
+
+while($e=mysql_fetch_assoc($q))
+       $output[]=$e;
+
+function jsonRemoveUnicodeSequences($struct) {
+   return @preg_replace("/\\\\u([a-f0-9]{4})/e", "iconv('UCS-4LE','UTF-8',pack('V', hexdec('U$1')))", json_encode($struct));
+}   
+   
+//print(json_encode($output));
+print jsonRemoveUnicodeSequences($output);
+mysql_close();
+?>

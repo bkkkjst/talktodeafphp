176b6c32e35322830bd27ea2cf6a36d8eb6ff757
bkkkjst <bkk_kjst02279@hotmail.com>
2015-02-19 01:57:44 +0700

    rename vocabulary_detail.php to

--- vocabulary_detail_by_name.php
+++ vocabulary_detail_by_name.php
@@ -1,6 +1,10 @@
 <?php
-mysql_connect("127.10.75.2:3306","adminqALkDCf","reUky8W-Z5DB");
-mysql_select_db("talktodeaf_db");
+include("connection.php");
+?>
+
+<?php
+mysql_connect($hostname,$username,$password);
+mysql_select_db($db_name);
 
 //ดึงข้อมูลออกมาในรูปแบบ UTF 8
 mysql_query("SET NAMES UTF8");
@@ -33,15 +37,18 @@ $q=mysql_query("SELECT\n".
 "vocabulary.des_id = description.id AND\n".
 "vocabulary.category_id = category.id");
 			
-while($e=mysql_fetch_assoc($q))
-       $output[]=$e;
+while($e=mysql_fetch_assoc($q)){
+	$output[]=$e;
+	print jsonRemoveUnicodeSequences($output);
+}
+       
 
 function jsonRemoveUnicodeSequences($struct) {
-   return @preg_replace("/\\\\u([a-f0-9]{4})/e", "iconv('UCS-4LE','UTF-8',pack('V', hexdec('U$1')))", json_encode($struct));
+   return preg_replace("/\\\\u([a-f0-9]{4})/e", "iconv('UCS-4LE','UTF-8',pack('V', hexdec('U$1')))", json_encode($struct));
 }   
    
 //print(json_encode($output));
-print jsonRemoveUnicodeSequences($output);
+
 mysql_close();
 ?>
 

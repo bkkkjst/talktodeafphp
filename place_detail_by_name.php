bad5812bf6f0d5aaa9caa0d84c62ffa9fb91e638
bkkkjst <bkk_kjst02279@hotmail.com>
2015-02-18 22:48:57 +0700

    edit place_detail.php

---
+++ place_detail_by_name.php
@@ -0,0 +1,47 @@
+<?php
+include("connection.php");
+?>
+
+<?php
+mysql_connect($hostname,$username,$password);
+mysql_select_db($db_name);
+
+//ดึงข้อมูลออกมาในรูปแบบ UTF 8
+mysql_query("SET NAMES UTF8");
+
+$place_name = $_GET['place_name'];
+
+$q=mysql_query("SELECT\n".
+"place.id,\n".
+"place.place_name,\n".
+"place.address,\n".
+"place.phone,\n".
+"place.latitude,\n".
+"place.longitude\n".
+"FROM\n".
+"place\n".
+"WHERE\n".
+"place.place_name = '{$place_name}'");
+while($e=mysql_fetch_assoc($q)){
+	
+	$place_detail = array();
+	
+	$place_detail["id"]=$e["id"];
+	$place_detail["place_name"]=$e["place_name"];
+	$place_detail["address"]=$e["address"];
+	$place_detail["phone"]=$e["phone"];
+	$place_detail["latitude"]=$e["latitude"];
+	$place_detail["longitude"]=$e["longitude"];
+   
+   //$output[]=$e;
+	print jsonRemoveUnicodeSequences($place_detail);
+ }
+ 
+function jsonRemoveUnicodeSequences($struct) {
+   return @preg_replace("/\\\\u([a-f0-9]{4})/e", "iconv('UCS-4LE','UTF-8',pack('V', hexdec('U$1')))", json_encode($struct));
+}   
+   
+//print(json_encode($output));
+
+mysql_close();
+?>

0128188a6bd3244e8f91e13b5150402e665dca55
bkkkjst <bkk_kjst02279@hotmail.com>
2015-02-19 22:56:56 +0700

    add game_random.php

--- game_random.php
+++ game_random.php
@@ -8,25 +8,41 @@ mysql_select_db($db_name);
 
 //ดึงข้อมูลออกมาในรูปแบบ UTF 8
 mysql_query("SET NAMES UTF8");
-$cat_name = $_GET['cat_name'];
+//$cat_name = $_GET['cat_name'];
 
 $q=mysql_query("SELECT\n".
-"category.cat_name\n".
+"vocabulary.id,\n".
+"vocabulary.voc_name,\n".
+"video.vid_name\n".
 "FROM\n".
-"category\n".
+"vocabulary ,\n".
+"video\n".
 "WHERE\n".
-"category.cat_name LIKE '%{$cat_name}' OR\n".
-"category.cat_name LIKE '{$cat_name}%' OR\n".
-"category.cat_name LIKE '%{$cat_name}%'");
+"vocabulary.video_id = video.id ");
 
-while($e=mysql_fetch_assoc($q))
-       $output[]=$e;
+$r=mysql_query("SELECT\n".
+"video.vid_name\n".
+"FROM\n".
+"video ");
+
+while(($e=mysql_fetch_assoc($q)) && ($f=mysql_fetch_assoc($r))){
+
+	$game = array();
+	/* $randdom_key1=array_rand($e,10);
+	$game[$randdom_key["id"]]; */
+	$game["id"]=$e["id"];
+	$game["voc_name"]=$e["voc_name"];
+	$game["right"]=$e["vid_name"];
+	$game["wrong"]=$f["vid_name"];
+	print jsonRemoveUnicodeSequences($game);
+}
+       //$output[]=$e;
 
 function jsonRemoveUnicodeSequences($struct) {
    return preg_replace("/\\\\u([a-f0-9]{4})/e", "iconv('UCS-4LE','UTF-8',pack('V', hexdec('U$1')))", json_encode($struct));
 }   
    
 //print(json_encode($output));
-print jsonRemoveUnicodeSequences($output);
+
 mysql_close();
 ?>

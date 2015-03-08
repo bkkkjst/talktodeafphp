c22dd4348ed46061a7f3bd8a261aab6f8b9af2c3
bkkkjst <bkk_kjst02279@hotmail.com>
2015-03-08 21:16:33 +0700

    book_1.php

--- book_1.php
+++ book_1.php
@@ -13,6 +13,7 @@ function jsonRemoveUnicodeSequences($struct) {
    return @preg_replace("/\\\\u([a-f0-9]{4})/e", "iconv('UCS-4LE','UTF-8',pack('V', hexdec('U$1')))", json_encode($struct));
 }   
    
+   
 //print(json_encode($output));
 print jsonRemoveUnicodeSequences($output);
 mysql_close();

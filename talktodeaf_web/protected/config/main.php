402e1ad35b0d572bb6989e9609c874578a83d392
bkkkjst <bkk_kjst02279@hotmail.com>
2015-03-16 19:03:41 +0700

    new web

--- talktodeaf_web/protected/config/main.php
+++ talktodeaf_web/protected/config/main.php
@@ -75,10 +75,10 @@ return array(
 		// uncomment the following to use a MySQL database
 		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=talktodeaf_db',
+			'connectionString' => 'mysql:host=127.10.75.2:3306;dbname=talktodeaf_db',
 			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
+			'username' => 'adminqALkDCf',
+			'password' => 'reUky8W-Z5DB',
 			'charset' => 'utf8',
 		),
 		

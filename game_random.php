f1395cc21a0d392e1cdc08c8197cff15ed6dfba5
bkkkjst <bkk_kjst02279@hotmail.com>
2015-05-08 23:55:38 +0700

    add speak video and edit file game_random not equal choice

--- game_random.php
+++ game_random.php
@@ -8,7 +8,9 @@ mysql_select_db($db_name);
 
 //ดึงข้อมูลออกมาในรูปแบบ UTF 8
 mysql_query("SET NAMES UTF8");
-//$cat_name = $_GET['cat_name'];
+
+
+$game = array();
 
 $q=mysql_query("SELECT\n".
 "vocabulary.voc_name,\n".
@@ -22,35 +24,70 @@ $q=mysql_query("SELECT\n".
 "ORDER BY RAND()\n".
 "LIMIT 1");
 
-$r=mysql_query("SELECT\n".
+$querySqlWrongAwnser = "SELECT\n".
 "vocabulary.voc_name,\n".
 "vocabulary.eng_voc_name\n".
 "FROM\n".
 "vocabulary\n".
 "ORDER BY RAND()\n".
-"LIMIT 1");
+"LIMIT 1";
+
+$r=mysql_query($querySqlWrongAwnser);
+
+// compare data is equal ??
+function isEqual($correct, $wrong){
+	
+	if($correct == $wrong){
+		return true;
+		}else {
+			return false;
+		}// end if
+	}// end function isEqual()
+
+
+
+// Query wrong awnser
+function queryWrongAwnser(){
+	global $game;
+	global $querySqlWrongAwnser;
+	
+	$r=mysql_query($querySqlWrongAwnser);
+
+		while($f=mysql_fetch_assoc($r)){
+			$game["eng_wrong"]=$f["eng_voc_name"];
+			$game["wrong"]=$f["voc_name"];
+			
+	isEqual($game["eng_wrong"], $game["wrong"]);		
+}// end while loop
+}// end function queryWrongAwnser()
 
 while(($e=mysql_fetch_assoc($q)) && ($f=mysql_fetch_assoc($r))){
 
-	$game = array();
-	/* $randdom_key1=array_rand($e,10);
-	$game[$randdom_key["id"]]; */
-	//$game["id"]=$e["id"];
+	
+	
 	$game["vid_name"]=$e["vid_name"];
 	
 	$game["eng_correct"]=$e["eng_voc_name"];
 	$game["eng_wrong"]=$f["eng_voc_name"];
 	$game["correct"]=$e["voc_name"];
 	$game["wrong"]=$f["voc_name"];
+	
+	
+	
+	while(isEqual($game["correct"], $game["wrong"])){
+		queryWrongAwnser();
+	}
+	
+	
+	
 	print jsonRemoveUnicodeSequences($game);
 }
-       //$output[]=$e;
+      
 
 function jsonRemoveUnicodeSequences($struct) {
    return preg_replace("/\\\\u([a-f0-9]{4})/e", "iconv('UCS-4LE','UTF-8',pack('V', hexdec('U$1')))", json_encode($struct));
 }   
-   
-//print(json_encode($output));
+   echo mysql_error();
 
 mysql_close();
 ?>
